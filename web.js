934c0e495059a709de49fa5e8113836307396f30
John Smith <andropologist@gmail.com>
2013-07-06 23:37:30 +0000

    77

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.get('/', function(request, response) {
 var fs = require('fs');
 var buf = new Buffer(fs.readFileSync('index.html'));
 var cont = buf.toString();
- response.send(buf.toString)
+ response.send(cont)
 });
 
 var port = process.env.PORT || 5000;
