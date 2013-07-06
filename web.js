e6c75d617a3cf8c4a9e8788d30d8155ba8f44661
John Smith <andropologist@gmail.com>
2013-07-06 22:24:03 +0000

    added var content

--- web.js
+++ web.js
@@ -2,9 +2,11 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var content = fs.readFileSync(index.html));
+
 
 app.get('/', function(request, response) {
-  response.send(buffer);
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
