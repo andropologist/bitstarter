d13702606e8efc8f54ef366391ec9946f99a926e
John Smith <andropologist@gmail.com>
2013-07-06 20:30:11 +0000

    changed the output

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+app.get(
+
+var buffer = new Buffer(fs.readFileSync(index.html,''), "utf-8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
