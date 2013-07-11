07eb57323691011db74cd681a0f1658fdaea2012
John Smith <andropologist@gmail.com>
2013-07-11 13:34:28 +0000

    updated grader.js to check url

--- grader.js
+++ grader.js
@@ -6,53 +6,72 @@ and basic DOM parsing.
 
 References:
 
- + cheerio
-   - https://github.com/MatthewMueller/cheerio
-   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
++ cheerio
+- https://github.com/MatthewMueller/cheerio
+- http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+- http://maxogden.com/scraping-with-node.html
 
- + commander.js
-   - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
++ commander.js
+- https://github.com/visionmedia/commander.js
+- http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
 
- + JSON
-   - http://en.wikipedia.org/wiki/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
++ JSON
+- http://en.wikipedia.org/wiki/JSON
+- https://developer.mozilla.org/en-US/docs/JSON
+- https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
+var rest = require('restler');
+var sys = require('util');
+
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+console.log("%s does not exist. Exiting.", instr);
+process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
 
 var cheerioHtmlFile = function(htmlfile) {
+    console.log("Function cheerioHtmlFile ran with: " + htmlfile);
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
 var loadChecks = function(checksfile) {
+    console.log("Function loadChecks ran with: " + checksfile);
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+// takes in a string of HTML and a checks file and tests it
+var checkHtml = function(html, checksfile) {
+    console.log("Function checkHtml ran with html:" + html + "and checksfile:" + checksfile);
+    $ = html;
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+var present = $(checks[ii]).length > 0;
+out[checks[ii]] = present;
     }
     return out;
+};
+
+var processFormat = function(htmlfile, url, checksfile) {
+    console.log("RAN: processFormat(" + htmlfile + ", " + url + ", " + checksfile);
+    if (!htmlfile) { // we are using url
+rest.get(url).on('complete', function(result) {
+console.log("rest.get is complete");
+var out = checkHtml(cheerio.load(result, checksfile));
+return out;
+});
+    } else {
+return checkHtml(cheerioHtmlFile(htmlfile), checksfile);
+    }
 };
 
 var clone = function(fn) {
@@ -63,12 +82,13 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+.option('-u, --url <url>', 'URL to check')
+.parse(process.argv);
+    var checkJson = processFormat(program.file, program.url, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+    exports.checkHtml = checkHtml;
 }
